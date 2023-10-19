9455e14c12251bec9d0fab9d49fddb99c59bd8d1
Sobuz <sobuz.info0298@gmail.com>
2023-10-19 15:53:26 +0600

    details data load

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
+require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-//BrandShop 
-//l1e6UKxfVTqlWbMj
-// console.log(process.env.DB_Pass)
 
-const uri = `mongodb+srv://BrandShop:l1e6UKxfVTqlWbMj@cluster0.sbw5eqf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.sbw5eqf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,15 +27,20 @@ async function run() {
     //data collection
     const dataCollection = client.db('brandShopDB').collection('data');
 
-    app.post('/data',async(req,res)=>{
-        const data = req.body;
-        const result = await dataCollection.insertOne(data)
-        res.send(result)
+   
+    app.get('/details/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = ({ _id: new ObjectId(id) })
+        const result = await dataCollection.findOne(query);
+        res.send(result);
     })
 
+  
+    //create api for all cetagory data 
     app.get('/data/:id',async(req,res)=>{
         const brandName = req.params.id;
-        const query = { brandName: brandName }
+        console.log(brandName)
+        const query = { brandName: brandName }  
         const cursor = dataCollection.find(query);
         const result = await cursor.toArray()
         res.send(result)
@@ -47,6 +50,12 @@ async function run() {
         const query = dataCollection.find();
 
         const result = await query.toArray()
+        res.send(result)
+    })
+
+    app.post('/data',async(req,res)=>{
+        const data = req.body;
+        const result = await dataCollection.insertOne(data)
         res.send(result)
     })
 
